c511cea132453fa8c76e91e2e8d095e2c1b41965
M Hamza Waqar <126167739+MHamzaWaqar56@users.noreply.github.com>
2026-04-02 07:00:33 +0500

    first commit

--- scripts/seed.js
+++ scripts/seed.js
@@ -26,7 +26,7 @@ async function seedAdmin() {
 
     // --- ADMIN DETAILS (Yahan apna email/password likhen) ---
     const adminEmail = "admin@alniazpetroleum.com";
-    const adminPassword = "admin123"; // Baad me change kr lena
+    const adminPassword = "Alniaz@123"; // Baad me change kr lena
     
     // -------------------------------------------------------
 
@@ -42,7 +42,7 @@ async function seedAdmin() {
 
     // Create Admin
     await User.create({
-      name: "Hamza Admin",
+      name: "Alniaz Petroleum",
       email: adminEmail,
       password: hashedPassword,
       role: "admin", // Ye zaroori ha
